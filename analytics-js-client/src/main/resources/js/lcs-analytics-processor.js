0ebf8b35879c4ddee028662e18b4d86e7bbcb57e
Eduardo Garcia <eduardo.garcia@liferay.com>
2017-11-02 13:17:37 +0100

    LPS-75189 This was required in Audience Targeting, but here it is not necessary

--- analytics-js-client/src/main/resources/js/lcs-analytics-processor.js
+++ analytics-js-client/src/main/resources/js/lcs-analytics-processor.js
@@ -19,53 +19,6 @@
 
 		var events = instance.getPendingEvents();
 
-		for (var k in events) {
-			var event = events[k];
-
-			var properties = event.properties;
-
-			properties.entityType = properties.className;
-
-			if (!isNaN(properties.classPK)) {
-				properties.entityId = Number(properties.classPK);
-			}
-
-			var referrers = properties.referrers;
-
-			for (var j in referrers) {
-				var referrer = referrers[j];
-				var referrerClassPKs = [];
-
-				referrer.referrerClassType = referrer.referrerClassName;
-
-				if (!isNaN(referrer.referrerClassPKs)) {
-					referrerClassPKs.push(Number(referrer.referrerClassPKs));
-				}
-				else {
-					var referrerClassPKsStrings = referrer.referrerClassPKs.split(',')
-
-					for (var p in referrerClassPKsStrings) {
-						referrerClassPKs.push(Number(referrerClassPKsStrings[p]));
-					}
-				}
-
-				referrer.referrerEntityIds = referrerClassPKs;
-
-				delete(referrer.referrerClassName);
-				delete(referrer.referrerClassPKs);
-
-				referrers[j] = referrer;
-			}
-
-			properties.referrers = referrers;
-
-			delete(properties.className);
-			delete(properties.classPK);
-
-			event.properties = properties;
-			events[k] = event;
-		}
-
 		pendingFlush = false;
 
 		if (events.length) {
