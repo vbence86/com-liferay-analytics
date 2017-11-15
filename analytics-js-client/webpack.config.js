21a0acca4a248a8de64326bf349e42d5e5ca5418
Bence Varga <vbence86@gmail.com>
2017-11-15 12:49:58 +0100

    LPS-75890 add Uglify plugin to webpack

--- analytics-js-client/webpack.config.js
+++ analytics-js-client/webpack.config.js
@@ -3,23 +3,30 @@ const buildFolder = './build';
 const buildName = 'analytis-all-min.js';
 
 module.exports = {
-  entry: './src/analytics.js',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          compact: false
-        }
-      }
-    }]
-  },
-  output: {
-    filename: `${buildFolder}/${buildName}`
-  },
-  plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin()
-  ]
+	entry: './src/analytics.js',
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						compact: false,
+					},
+				},
+			},
+		],
+	},
+	output: {
+		filename: `${buildFolder}/${buildName}`,
+	},
+	plugins: [
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
+		}),
+	],
 };
