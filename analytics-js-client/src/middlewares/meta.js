9ad8d59333ba7d9cc924dd4536349c6f0b5ce4e4
Bence Varga <vbence86@gmail.com>
2017-11-15 12:41:10 +0100

    fix linting errors

--- analytics-js-client/src/middlewares/meta.js
+++ analytics-js-client/src/middlewares/meta.js
@@ -1,10 +1,10 @@
 import LCSClient from '../LCSClient';
 
 function getQuery(query, attr) {
-  return function() {
-    const tag = document.querySelector(query) || {};
-    return tag[attr];
-  }
+	return function() {
+		const tag = document.querySelector(query) || {};
+		return tag[attr];
+	};
 }
 
 const getDescription = getQuery('meta[name="description"]', 'content');
@@ -12,17 +12,16 @@ const getKeywords = getQuery('meta[name="keywords"]', 'content');
 const getTitle = getQuery('title', 'innerHTML');
 
 function defaultMiddleware(req, analytics) {
-  req.context = {
-    description: getDescription(),
-    keywords: getKeywords(),
-    languageId: navigator.language,
-    title: getTitle(),
-    url: location.href,
-    userAgent: navigator.userAgent,
-    ...req.context
-  };
-  return req;
+	req.context = {
+		description: getDescription(),
+		keywords: getKeywords(),
+		languageId: navigator.language,
+		title: getTitle(),
+		url: location.href,
+		userAgent: navigator.userAgent,
+		...req.context
+	};
+	return req;
 }
 
 LCSClient.use(defaultMiddleware);
-    
