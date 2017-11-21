43320cc804f242cdbfbb77c0bf16d88079883f5b
Bence Varga <vbence86@gmail.com>
2017-11-21 11:01:18 +0100

    remove comments from test file

--- analytics-js-client/test/analytics.test.js
+++ analytics-js-client/test/analytics.test.js
@@ -108,7 +108,6 @@ describe('Analytics API', () => {
 	});
 
 	it('No overlapping requests are allowed', function() {
-		// every 2s we autoflush the storage
 		const AUTO_FLUSH_FREQUENCY = 2000;
 		let fetchCalled = 0;
 
@@ -124,26 +123,18 @@ describe('Analytics API', () => {
 		Analytics.create({
 			autoFlushFrequency: AUTO_FLUSH_FREQUENCY
 		});
-
-		// generates few events
+		
 		sendDummyEvents();
 
-		// spies against Analytics.flush function
 		const spy = sinon.spy(Analytics, 'flush');
 
 		return new Promise(resolve => {
-			// it waits 3 loop cycle to make sure there is no further
-			// attempts to flush the storage since the first request has not yet
-			// been processed
+
 			setTimeout(() => {
 
-				// flush must be called 3 times
 				assert.isTrue(spy.calledThrice);
-
-				// but without sending another Fetch Request
 				expect(fetchCalled).to.equal(1);
 
-				// tears down the test gracefully
 				Analytics.flush.restore();
 				resolve();
 
